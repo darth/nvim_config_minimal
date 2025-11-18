dd060eb92cc8d12be7f00c16d6bafbf661abb1cf
Vadim Alimguzhin <vadim.alimguzhin@gmail.com>
2025-11-18 19:30:40 +0100

    Configure nvim_lsp for lua-language-server.

--- init.lua
+++ init.lua
@@ -8,8 +8,8 @@ require("paq")({
   "nvim-tree/nvim-web-devicons",
   "nvim-lualine/lualine.nvim",
   "ibhagwan/fzf-lua",
-  "neovim/nvim-lspconfig",
   "nvim-treesitter/nvim-treesitter",
+  "neovim/nvim-lspconfig",
 })
 
 vim.opt.shortmess:append({ I = true })
@@ -153,3 +153,55 @@ require("fzf-lua").setup({
   },
 })
 vim.keymap.set("n", "<leader>f", "<cmd>FzfLua files<CR>")
+
+-- lspconfig
+vim.lsp.enable("lua_ls")
+vim.lsp.config("lua_ls", {
+  on_init = function(client)
+    if client.workspace_folders then
+      local path = client.workspace_folders[1].name
+      if
+        path ~= vim.fn.stdpath("config")
+        and (vim.uv.fs_stat(path .. "/.luarc.json") or vim.uv.fs_stat(path .. "/.luarc.jsonc"))
+      then
+        return
+      end
+    end
+
+    client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most
+        -- likely LuaJIT in the case of Neovim)
+        version = "LuaJIT",
+        -- Tell the language server how to find Lua modules same way as Neovim
+        -- (see `:h lua-module-load`)
+        path = {
+          "lua/?.lua",
+          "lua/?/init.lua",
+        },
+      },
+      -- Make the server aware of Neovim runtime files
+      workspace = {
+        checkThirdParty = false,
+        library = {
+          vim.env.VIMRUNTIME,
+          "${3rd}/luv/library",
+        }
+      }
+    })
+  end,
+  settings = {
+    Lua = {
+      format = {
+        enable = true,
+        defaultConfig = {
+          indent_style = "space",
+          indent_size = "2",
+          quote_style = "double",
+          trailing_table_separator = "keep",
+          call_arg_parentheses = "remove",
+        }
+      },
+    }
+  }
+})
